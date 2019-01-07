5c14659be33db93e03439bc9edb1f88b00efe5bb
tehnomanik <32140776+tehnomanik@users.noreply.github.com>
2019-01-07 23:24:19 +0100

    Update nodeMCU-wifiConfig.ino

--- nodeMCU-wifiConfig.ino
+++ nodeMCU-wifiConfig.ino
@@ -1,3 +1,4 @@
+
 //Needs to be flashed 2 times. First time uncomment clearEEPROM(); line in setup function. This will write
 //default data to EEPROM, before reading from it. Now comment or delete this line from sketch and reflash nodeMCU board.
 //This is simple example of using web page as user interface to configure wifi parameters for nodeMCU board to connect to 
@@ -14,7 +15,9 @@
 #include <EEPROM.h>
 
 bool isActivated = false; // flag for AP mode, if isActivate = true, board connects as wifi client
-int resretButton = 0; // pin number with RESET button when pressed clears stored information and activates AP mode
+
+int resetButton = 0; // pin number with RESET button when pressed clears stored information and activates AP mode
+int LEDpin = 2; // nodeMCU onboard LED
 
 uint addr = sizeof(isActivated); // EEPROM address of node_config object
 
@@ -33,18 +36,19 @@ ESP8266WebServer server(80);
 void setup() {
   // put your setup code here, to run once:
   pinMode(resetButton, INPUT_PULLUP); // FLASH button on nodeMCU board, used to clear saved user values, and set board to boot in Access Point mode
+  pinMode(LEDpin, OUTPUT); // onboard LED
+  digitalWrite(LEDpin, HIGH); // turn off onboard LED
   
   EEPROM.begin(512); // start EEPROM functions
-  
-  // uncomment next line for first flash, then comment it again and reflash (EEPROM must be written before reading from it)
-  
-  // clearEEPROM(); 
+
+  // clearEEPROM(); // uncomment for first flash, then comment again and reflash
   
   EEPROM.get(0, isActivated); // get first value stored in EEPROM and assign it to isActivated variable which indicates if board will boot in client or AP wireless mode
 
   // If board is not activated start in AP mode, default ip is 192.168.4.1
-  if (isActivated == false)
+  if (isActivated != true)
   { 
+    clearEEPROM();
     WiFi.mode(WIFI_AP);
     WiFi.softAP("nodemcuwifi", "nodemcuwifi");
     delay(1000);
@@ -83,6 +87,7 @@ void loop() {
   if (digitalRead(0) == LOW)
   {
     clearEEPROM();
+    digitalWrite(LEDpin, LOW); // turn ON onboard LED indicating that EEPROM is cleared
   }
   server.handleClient();
   delay(50);
@@ -128,7 +133,7 @@ void handleEEPROM()
 // getTemp page
 String getDefaultPage() {
   
-  String page = "<!DOCTYPE html>\n<html>\n<head>\n <title>WIFI CONFIGURATION</title>\n <meta charset=\"utf-8\" />\n <meta name=\"viewport\" content=\"width=600, user-scalable=no\" />\n\n <style>\n body {\n background-color: #fff;\n margin: auto;\n font-family: sans-serif;\n font-weight: bold;\n text-decoration: none !important;\n text-align: center;\n }\n\n a {\n text-decoration: none;\n }\n\n .container {\n display: block;\n width: 600px;\n clear: both;\n margin: auto;\n }\n\n /* ROWS */\n .row {\n display: block;\n height: 80px;\n clear: both;\n vertical-align: middle;\n line-height: 80px;\n border-bottom: 2px solid #1a1a1a;\n overflow: hidden;\n font-size: 30px;\n }\n .row-xs {\n height: 40px;\n line-height: 40px;\n font-size: 14px;\n } \n\n .row-sm {\n height: 60px;\n line-height: 60px;\n font-size: 24px;\n }\n\n .row-lg {\n height: 120px;\n line-height: 120px;\n font-size: 36px;\n }\n\n /* CELLS */\n\n .cell-lg {\n width: 600px;\n float: left;\n }\n\n .cell-md {\n width: 400px;\n float: left;\n }\n\n .cell-sm {\n width: 200px;\n float: left;\n text-align: center;\n }\n\n .indicator {\n display: block;\n width: 60px;\n float: left;\n height: 100%;\n text-align: center;\n }\n\n .btn {\n width: 180px;\n height: 100%;\n float: left;\n font-size: 24px;\n font-weight: bold;\n border-left: 10px solid #000;\n border-right: 10px solid #000;\n background-color: #0094ff;\n color: white;\n }\n\n /*colors and backrounds*/\n .bg-info {\n background-color: #e6e6e6;\n color: #1a1a1a;\n }\n\n .bg-data {\n background-color: #0c336e;\n color: white;\n }\n\n .bg-title {\n background-color: #4e4e4e;\n color: white;\n }\n\n /* indicator styles */\n .on {\n background-color: #59e430;\n color: #59e430;\n }\n\n .off {\n color: #ff0000;\n background-color: #ff0000;\n }\n /* Empty Cell */\n .empty {\n background-color: #fff;\n color: #fff;\n }\n .big {\n font-size: 72px;\n font-weight: bold;\n }\n .text-left {\n text-align: left;\n padding-left: 12px;\n }\n .textbox {\n text-align: center;\n font-size: 32px;\n width: 80%;\n }\n .textbox-ip {\n text-align: center;\n font-size: 32px;\n width: 65px;\n }\n .btn-in {\n width: 200px;\n height: 120px;\n float: left;\n font-size: 32px;\n font-weight: bold;\n }\n .active {\n background-color: darkorange;\n }\n </style>\n\n</head>\n<body>\n\n <div class=\"container\">\n\n <div class=\"row bg-title\">SETTINGS</div>\n\n <form action=\"/saveEEPROM\" method=\"POST\"> \n \n <div class=\"row row-sm text-left\">WiFi Configuration</div>\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">SSID</div>\n <div class=\"cell-md\"><input type=\"text\" class=\"textbox\" name=\"wifi_ssid\" value=\"##wifi_ssid##\"></div>\n </div>\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">WiFi Pass</div>\n <div class=\"cell-md\"><input type=\"text\" class=\"textbox\" name=\"wifi_password\" value=\"##wifi_password##\"></div>\n </div>\n\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">IP address</div>\n <div class=\"cell-md\">\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip1\" value=\"##wifi_ip1##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip2\" value=\"##wifi_ip2##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip3\" value=\"##wifi_ip3##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip4\" value=\"##wifi_ip4##\">\n </div>\n </div>\n\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">Gateway</div>\n <div class=\"cell-md\">\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw1\" value=\"##wifi_gw1##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw2\" value=\"##wifi_gw2##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw3\" value=\"##wifi_gw3##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw4\" value=\"##wifi_gw4##\">\n </div>\n </div>\n\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">Subnet</div>\n <div class=\"cell-md\">\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub1\" value=\"##wifi_sub1##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub2\" value=\"##wifi_sub2##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub3\" value=\"##wifi_sub3##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub4\" value=\"##wifi_sub4##\">\n </div>\n </div>\n\n <div class=\"row row-lg\">\n <div class=\"cell-sm empty\">.</div>\n <div class=\"cell-sm\"><input type=\"submit\" class=\"btn-in bg-info\" value=\"SAVE\"></div>\n <a href=\"#\" class=\"cell-sm\"></a>\n </div>\n </form>\n\n</body>\n</html>\n";
+  String page = "<!DOCTYPE html>\n<html>\n<head>\n <title>pumpMaster - SETTINGS</title>\n <meta charset=\"utf-8\" />\n <meta name=\"viewport\" content=\"width=600, user-scalable=no\" />\n\n <style>\n body {\n background-color: #fff;\n margin: auto;\n font-family: sans-serif;\n font-weight: bold;\n text-decoration: none !important;\n text-align: center;\n }\n\n a {\n text-decoration: none;\n }\n\n .container {\n display: block;\n width: 600px;\n clear: both;\n margin: auto;\n }\n\n /* ROWS */\n .row {\n display: block;\n height: 80px;\n clear: both;\n vertical-align: middle;\n line-height: 80px;\n border-bottom: 2px solid #1a1a1a;\n overflow: hidden;\n font-size: 30px;\n }\n .row-xs {\n height: 40px;\n line-height: 40px;\n font-size: 14px;\n } \n\n .row-sm {\n height: 60px;\n line-height: 60px;\n font-size: 24px;\n }\n\n .row-lg {\n height: 120px;\n line-height: 120px;\n font-size: 36px;\n }\n\n /* CELLS */\n\n .cell-lg {\n width: 600px;\n float: left;\n }\n\n .cell-md {\n width: 400px;\n float: left;\n }\n\n .cell-sm {\n width: 200px;\n float: left;\n text-align: center;\n }\n\n .indicator {\n display: block;\n width: 60px;\n float: left;\n height: 100%;\n text-align: center;\n }\n\n .btn {\n width: 180px;\n height: 100%;\n float: left;\n font-size: 24px;\n font-weight: bold;\n border-left: 10px solid #000;\n border-right: 10px solid #000;\n background-color: #0094ff;\n color: white;\n }\n\n /*colors and backrounds*/\n .bg-info {\n background-color: #e6e6e6;\n color: #1a1a1a;\n }\n\n .bg-data {\n background-color: #0c336e;\n color: white;\n }\n\n .bg-title {\n background-color: #4e4e4e;\n color: white;\n }\n\n /* indicator styles */\n .on {\n background-color: #59e430;\n color: #59e430;\n }\n\n .off {\n color: #ff0000;\n background-color: #ff0000;\n }\n /* Empty Cell */\n .empty {\n background-color: #fff;\n color: #fff;\n }\n .big {\n font-size: 72px;\n font-weight: bold;\n }\n .text-left {\n text-align: left;\n padding-left: 12px;\n }\n .textbox {\n text-align: center;\n font-size: 32px;\n width: 80%;\n }\n .textbox-ip {\n text-align: center;\n font-size: 32px;\n width: 65px;\n }\n .btn-in {\n width: 200px;\n height: 120px;\n float: left;\n font-size: 32px;\n font-weight: bold;\n }\n .active {\n background-color: darkorange;\n }\n </style>\n\n</head>\n<body>\n\n <div class=\"container\">\n\n <div class=\"row bg-title\">SETTINGS</div>\n\n <form action=\"/saveEEPROM\" method=\"POST\"> \n \n <div class=\"row row-sm text-left\">WiFi Configuration</div>\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">SSID</div>\n <div class=\"cell-md\"><input type=\"text\" class=\"textbox\" name=\"wifi_ssid\" value=\"##wifi_ssid##\"></div>\n </div>\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">WiFi Pass</div>\n <div class=\"cell-md\"><input type=\"text\" class=\"textbox\" name=\"wifi_password\" value=\"##wifi_password##\"></div>\n </div>\n\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">IP address</div>\n <div class=\"cell-md\">\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip1\" value=\"##wifi_ip1##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip2\" value=\"##wifi_ip2##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip3\" value=\"##wifi_ip3##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_ip4\" value=\"##wifi_ip4##\">\n </div>\n </div>\n\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">Gateway</div>\n <div class=\"cell-md\">\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw1\" value=\"##wifi_gw1##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw2\" value=\"##wifi_gw2##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw3\" value=\"##wifi_gw3##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_gw4\" value=\"##wifi_gw4##\">\n </div>\n </div>\n\n <div class=\"row row-sm bg-info\">\n <div class=\"cell-sm\">Subnet</div>\n <div class=\"cell-md\">\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub1\" value=\"##wifi_sub1##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub2\" value=\"##wifi_sub2##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub3\" value=\"##wifi_sub3##\">.\n <input type=\"text\" class=\"textbox-ip\" name=\"wifi_sub4\" value=\"##wifi_sub4##\">\n </div>\n </div>\n\n <div class=\"row row-lg\">\n <div class=\"cell-sm empty\">.</div>\n <div class=\"cell-sm\"><input type=\"submit\" class=\"btn-in bg-info\" value=\"SAVE\"></div>\n <a href=\"#\" class=\"cell-sm\"></a>\n </div>\n </form>\n\n</body>\n</html>\n";
 
   EEPROM.get(addr, node_config);
 
@@ -152,9 +157,7 @@ String getDefaultPage() {
 
   return page;
 }
-// End Default page
 
-// Sets default values and sets board to boot in AP mode
 void clearEEPROM()
 {
   isActivated = false;
@@ -168,4 +171,4 @@ void clearEEPROM()
   EEPROM.put(addr, node_config);
   EEPROM.commit();
 }
-
+// End Default page
