8c3dc7024a3289836875df2448a6a572cdf77995
tehnomanik <32140776+tehnomanik@users.noreply.github.com>
2021-09-24 22:00:20 +0200

    Add files via upload

---
+++ arduino-nrf24/NRF24-receiver.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7,8);
+
+const int buzzer = 2;
+const byte address[6] = "00001";
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buzzer, OUTPUT);
+  radio.begin();
+  radio.openReadingPipe(0,address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.startListening();
+}
+
+void loop() {
+  char text[32] = "";
+  
+  if (radio.available()) {
+    radio.read(&text, sizeof(text));
+    String transData = String(text);
+    if (transData == "nrftest") {
+        tone(buzzer, 1000);
+        delay(500);
+        noTone(buzzer);
+      }    
+  }
+}
---
+++ arduino-nrf24/NRF24-transmitter.ino
@@ -0,0 +1,21 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7,8);
+
+const byte address[6] = "00001";
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.stopListening();
+}
+
+void loop() {
+  const char text[] = "nrftest";
+  radio.write(&text, sizeof(text));
+  delay(2000);
+}
